8f7528610e9689de8db1b6ca034dbfbf6e67c45c
DavidV <d.veltzer@gmail.com>
2021-09-06 16:14:39 +0300

    removed unused components and imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import MainContainerComp from "./_components/MainContainer";
 import Container from "@material-ui/core/Container";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-
-import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<CssBaseline />
 			<Container maxWidth={"sm"}>
 				<React.StrictMode>
 					<MainContainerComp />
